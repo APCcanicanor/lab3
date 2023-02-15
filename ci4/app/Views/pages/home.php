715cd033e56970c268468fc1772e73f16e184dea
Carl Von A. Nicanor <canicanor@student.apc.edu.ph>
2023-02-15 14:06:14 +0800

    Convert your Personal Profile website to CodeIgniter 4

---
+++ ci4/app/Views/pages/home.php
@@ -0,0 +1,43 @@
+<nav class="custom-header">
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+              <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo"><a href="#home" style="color: white;">V's</a></label>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#w3">w3schools</a></li>
+        </ul>
+    </nav>
+
+<!--SECTION 1-->
+    <section class="home-area" id="home">
+        <div id="text-part">
+            <h1 style="margin-top: 15%">
+            To get to know me <br> 
+            Click the button to learn more
+            </h1>
+            <a href="personal profile.html"><button class="h-btn">Learn More</button></a>
+        </div>
+    </section>
+ <!--Section 2-->
+ <section class="w3-area" id="w3">
+    <div class="text-part" style="margin-top: 10%">
+        <h1>W3SCHOOLS</h1>
+        <p><a href="w3schools.html" class="w3-button">View</a></p>
+    </div>
+ </section>
+ <hr>
+
+  <!--Footer-->
+  <footer>
+    <div class="footer-content">
+        <h3>V's Website</h3>
+        <p>Follow us on:</p>
+        <ul class="socials">
+            <li><a href="https://www.facebook.com/carlvon.nicanor/"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com/caii522"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/carl-von-nicanor-3754b818b/"><i class="fa fa-linkedin-square"></i></a></li>
+        </ul>
+    </div>
+</footer>
