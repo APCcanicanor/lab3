f8fdad5b4812b6b68977015cb9504d95354e15af
Carl Von A. Nicanor <canicanor@student.apc.edu.ph>
2023-03-06 22:03:52 +0800

    Update index.php

--- ci4/app/Views/guest/index.php
+++ ci4/app/Views/guest/index.php
@@ -10,12 +10,12 @@
 		<table>
 		<tr>
 		<th>-----</th>
-		 <th><?= esc($guest_item['name']) ?> <br> </th>
-         <th><?= esc($guest_item['email']) ?> <br> </th>
-         <th><?= esc($guest_item['website']) ?> <br> </th>
-         <th><?= esc($guest_item['gender']) ?> <br> </th>
-		 <th><?= esc($guest_item['comment']) ?> <br> </th>
-         <th><?= esc($guest_item['reg_date']) ?> <br> </th>
+		 <th><?= esc($guest_item['name']) ?> </th>
+         <th><?= esc($guest_item['email']) ?> </th>
+         <th><?= esc($guest_item['website']) ?> </th>
+         <th><?= esc($guest_item['gender']) ?> </th>
+		 <th><?= esc($guest_item['comment']) ?> </th>
+         <th><?= esc($guest_item['reg_date']) ?> </th>
 		</tr>
 		</table>
         </div>
